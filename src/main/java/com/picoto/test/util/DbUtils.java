57debf18f039f8c4a8983669f1397b240ffc3ee3
jmgodino <jmgodino@local.com>
2023-06-08 09:10:40 +0200

    fixex sonar

--- src/main/java/com/picoto/test/util/DbUtils.java
+++ src/main/java/com/picoto/test/util/DbUtils.java
@@ -8,12 +8,14 @@ import com.picoto.test.exceptions.TestException;
 
 public class DbUtils {
 
+	private DbUtils() {
+	}
+
 	public static Connection getConnection(String name) {
 		try {
 			InitialContext cxt = new InitialContext();
 			DataSource ds = (DataSource) cxt.lookup(String.format("java:/comp/env/jdbc/%s", name));
-			Connection con = ds.getConnection();
-			return con;
+			return ds.getConnection();
 		} catch (Exception e) {
 			throw new TestException(String.format("Error al conectar a BBDD %s",e.getMessage()));
 		}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,9 @@
-<html>
+<DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Test</title>
+	</head>
 <body>
 Test activo!
 </body>
